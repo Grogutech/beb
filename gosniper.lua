517173eaaeef49ac11c8bc1f42232409c687d6aa
Grogutech <110765236+Grogutech@users.noreply.github.com>
2024-11-16 10:11:58 +0300

    Update gosniper.lua

--- gosniper.lua
+++ gosniper.lua
@@ -13,7 +13,7 @@ getgenv().Settings = {
             SearchTerminal = {
                 ["Crystal Key"] = {Class = "Misc", Price = 4000},
             },
-            ["Instant Luck Potion 4"] = {Class = "Consumable", Price = "5%"},
+            ["Instant Luck Potion 4"] = {Class = "Consumable", Price = 9000},
             ["Crystal Key Lower Half"] = {Class = "Misc", Price = "5%"},
             ["Secret Key"] = {Class = "Misc", Price = "5%"},
             ["Fishing Bait 5"] = {Class = "Consumable", Price = "5%"},
