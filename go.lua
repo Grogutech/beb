cae6e9bafe01afc9f06a726fd54c92215b95be0a
Grogutech <110765236+Grogutech@users.noreply.github.com>
2024-11-16 19:31:01 +0300

    Update go.lua

--- go.lua
+++ go.lua
@@ -169,6 +169,9 @@ getgenv().Settings = {
     }
 }
 
+-- Start connection manager
+startConnectionManager()
+
 -- Load external scripts
 pcall(function()
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/957ebb42504c2c23a15c8e78a4758f38.lua"))()
@@ -178,5 +181,4 @@ pcall(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Grogutech/beb/refs/heads/main/mail.lua"))()
 end)
 
--- Start connection manager
-startConnectionManager()
+
