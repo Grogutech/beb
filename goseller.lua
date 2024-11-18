60f9d5ad4df88423d1b389c1016151388e97d840
Grogutech <110765236+Grogutech@users.noreply.github.com>
2024-11-18 04:49:16 +0300

    Update goseller.lua

--- goseller.lua
+++ goseller.lua
@@ -53,7 +53,7 @@ getgenv().Settings = {
             ["Remove Username"] = true,
         },
         StopParams = {
-            ["Item Runout"] = true,
+            ["Item Runout"] = false,
             ["Diamonds Hit: 1b"] = false,
             ["60 Minutes"] = false,
             ["Switch To Sniping"] = false,
