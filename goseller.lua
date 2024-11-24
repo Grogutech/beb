69a6d9d09248de341b793fedc0af639a3ddd9c05
Grogutech <110765236+Grogutech@users.noreply.github.com>
2024-11-24 21:42:13 +0300

    Update goseller.lua

--- goseller.lua
+++ goseller.lua
@@ -34,7 +34,7 @@ getgenv().Settings = {
             ["RAP Above: 500"]= {Class = "Pet", Price = "-5%"},
             --["Crystal Key"] = {Class = "Misc", Price = 6500},
             ["Crystal Key Upper Half"] = {Class = "Misc", Price = 3000},
-            ["All Items"] = {Class = "Lootbox", Price = "25%"},
+            --["All Items"] = {Class = "Lootbox", Price = "25%"},
         },
         Serverhop = {
             ["Switch Servers"] = true,
