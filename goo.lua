44339e25f97a327b08f2dfe26d952a883fbcd6a0
Grogutech <110765236+Grogutech@users.noreply.github.com>
2025-04-18 23:54:43 +0300

    Update goo.lua

--- goo.lua
+++ goo.lua
@@ -9,11 +9,15 @@ getgenv().Settings = {
         Webhook = "https://discord.com/api/webhooks/1362892290607022281/5WMRCxKCjOZpwTkMV0W37KenhGLFlVQPqW1wKOFP-Bden4TSbX-8NVIUScmTu18LHaCM",
         ["Pets"] = {
             KeepBestPets = true,
-            Difficulty = "Above 10000m",
+            Difficulty = "Above 1b",
         },
         ["Items"] = {
             --// These are EXAMPLES, please change/add anything to your liking! \\--
-            ["Diamond Fishing Rod"] = {Amount = 1},
+            ["Exotic Mining Chest"] = {Amount = 1},
+            ["Runic Mining Chest"] = {Amount = 1},
+            ["Abyssal Treasure Chest"] = {Amount = 1},
+            ["Instant Luck Potion 4"] = {Amount = 1},
+            ["Valentines Spinny Wheel Ticket"] = {Amount = 1} 
         },
     },
     ["Debug"] = {
