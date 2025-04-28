d25cea7531fb1ec1b6f7051c4745e30ad08e100b
Grogutech <110765236+Grogutech@users.noreply.github.com>
2025-04-28 07:58:55 +0300

    Update gum.lua

--- gum.lua
+++ gum.lua
@@ -1,15 +1,49 @@
-getgenv().Config = {
-    ["Hatching"] = {
-        ["Rifts"] = {
-            ["Eggs"] = {"Silly Egg", "Throwback Egg", "Nightmare Egg"}
+getgenv().Settings = {
+    ["Egg Settings"] = {
+        OpenEggs = true,
+        Egg = "100M Egg",
+        --// Supports ANY egg ("Rainbow Egg", "Infinity Egg", ...). Keep "Best" for new accounts.
+        
+        ["Notifications"] = {
+            Webhook = dc_webhook,
+            DiscordID = "314107374715535370",
+            Difficulty = "100k", --// Minimum Difficulty for Webhook Notifications
         },
-        ["Egg"] = "100M Egg"
+
+        ["Rifts"] = {
+            FindRifts = true,
+            SortByMultiplier = true, 
+            --// true --> Sort by Multiplier out of ALL Targetted Rifts.
+            --// false --> Sort by Multiplier out of BEST Targetted Rifts.
+            
+            Targets = {},
+            --// Targets = {} will automatically find the Top 3 BEST Rifts to hatch.
+        },
     },
-    ["Webhook"] = {
-        ["MinDifficulty"] = "100k",
-        ["UserID"] = "314107374715535370",
-        ["URL"] = dc_webhook,
+    
+    ["Enchant Settings"] = {
+        EnchantPets = false,
+        
+        ["Require All Enchants"] = true,
+        ["Enchants Needed"] = {
+            ["Team Up"] = { Tier = 5, HigherTiers = true },
+        },
     },
-    ["AutoEnchant"] = true -- // Enchant Team Up on all Equipped Pets
+    
+    ["Debug"] = {
+        DisableUI = false,
+        DisableUseMysteryBox = false,
+        DisableUseGoldenBox = false,
+        DisableUseSeason1Egg = false,
+        DisableAutoDelete = false,
+        DisableEquipPets = false,
+        DisablePurchaseMerchants = false,
+        DisableCraftPotions = false,
+        SellBubbles = true,
+        FarmCompetitiveQuests = true,
+        --MinAccountMastery = 20,
+        MinFarmTime = 4,
+        DisableUseInfinity = false,
+    },
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6a28683e5e681161a5074613f6daf64c.lua"))()
+loadstring(game:HttpGet("https://system-exodus.com/scripts/BGSI/OneClick.lua"))()
