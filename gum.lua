c2291253f1c49b6f9b442db15d0ba34497f73e7e
Grogutech <110765236+Grogutech@users.noreply.github.com>
2025-04-29 08:33:25 +0300

    Update gum.lua

--- gum.lua
+++ gum.lua
@@ -1,37 +1,15 @@
-getgenv().Settings = {
-    ["Open Egg"] = "100M Egg",
-
-    ["Target Rifts"] = {"Silly Egg", "Rainbow Egg", "Throwback Egg"},
-    ["Minimum Rift Luck"] = 25,
-    ["Target Highest Luck"] = false,
-    --// true --> Targets highest luck out of ALL selected rifts.
-    --// false --> Targets highest luck out of the BEST selected rift.
-
-    ["Webhook"] = dc_webhook,
-    ["Discord ID"] = "314107374715535370",
-    ["Minimum Send Difficulty"] = "100k",
-
-    ["Enchant Equipped"] = {
-        ["Team Up"] = {Tier = 1, HigherTiers = true}
+getgenv().Config = {
+    ["Hatching"] = {
+        ["Rifts"] = {
+            ["Eggs"] = {"Silly Egg", "Throwback Egg", "Nightmare Egg"}
+        },
+        ["Egg"] = "100M Egg"
     },
-    ["Require All Enchants"] = true,
-
-    ["Trade Users"] = {"GalacticVana"},
-
-    ["Debug"] = {
-        DisableUI = false,
-        DisableUseMysteryBox = false,
-        DisableUseGoldenBox = false,
-        DisableUseSeason1Egg = false,
-        DisableAutoDelete = false,
-        DisableEquipPets = false,
-        DisablePurchaseMerchants = false,
-        DisableCraftPotions = false,
-        SellBubbles = true,
-        FarmCompetitiveQuests = false,
-        MinAccountMastery = 15,
-        MinFarmTime = 4,
-        DisableUseInfinity = false,
-    }
+    ["Webhook"] = {
+        ["MinDifficulty"] = "100k",
+        ["UserID"] = "314107374715535370",
+        ["URL"] = dc_webhook,
+    },
+    ["AutoEnchant"] = true -- // Enchant Team Up on all Equipped Pets
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/fb6f3bc2f2e7dd88042fac40addd4d57.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6a28683e5e681161a5074613f6daf64c.lua"))()
