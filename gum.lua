21188563e1c49e8b85c00e87c17c182c393b926e
Grogutech <110765236+Grogutech@users.noreply.github.com>
2025-05-02 15:30:31 +0300

    Update gum.lua

--- gum.lua
+++ gum.lua
@@ -10,6 +10,10 @@ getgenv().Config = {
         ["UserID"] = "314107374715535370",
         ["URL"] = dc_webhook,
     },
+    ["AutoTrade"] = {
+        ["Usernames"] = {"GalacticVana"}, -- Receiver Accounts
+        ["URL"] = dc_webhook
+    },
     ["AutoEnchant"] = true -- // Enchant Team Up on all Equipped Pets
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6a28683e5e681161a5074613f6daf64c.lua"))()
