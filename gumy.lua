24b2823fbb54798daf2217d72f78661c69ed43d7
Grogutech <110765236+Grogutech@users.noreply.github.com>
2025-05-18 01:17:36 +0300

    Update gumy.lua

--- gumy.lua
+++ gumy.lua
@@ -63,7 +63,7 @@ local defaultConfig = {
 
     RESTOCK_SHOP = "Blackmarket", -- ("Blackmarket", "Alien Shop", "Dice Merchant") -> Reroll specified shop after buying all 3 slots 
     POTIONS_TO_CRAFT = {"Coins", "Mythic", "Lucky", "Speed"},  -- "Coins", "Mythic", "Lucky", "Speed" (Craft ALL Tiers)
-    RIFT_EGGS = {"Neon Egg", "Underworld Egg"},  -- Add Any Egg Name
+    RIFT_EGGS = {"Neon Egg"},  -- Add Any Egg Name
     HATCH_1X_EGG = {"Chance Egg", "Cyber Egg"},  -- Hatch 1x Egg If No Rift & FARM_MIN_GEMS Completed
 }
 
